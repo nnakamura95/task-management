003e1d5dc50a35b2e8bfc6c0b5a0b398f83abf53
Nobumichi Leo Nakamura <we.are.one779@gmail.com>
2022-03-28 09:43:05 -0400

    Implement TaskMapper

---
+++ app-server/src/main/java/com/app/api/datasource/TaskMapper.java
@@ -0,0 +1,20 @@
+package com.app.api.datasource;
+
+import com.app.api.domain.Task;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface TaskMapper {
+
+    Task getTask(long id);
+
+    List<Task> getAllTask();
+
+    void saveNewTask(Task task);
+
+    void deleteTask(int id);
+
+    void updateTask(Task task);
+}
