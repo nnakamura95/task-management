c1e69765b9d3549c1a832a2932279211a353d85c
Nobumichi Leo Nakamura <we.are.one779@gmail.com>
2022-03-28 09:49:26 -0400

    Created flyway sql migration: create task table

---
+++ app-server/src/main/resources/db/migration/versions/V1__tasks.sql
@@ -0,0 +1,18 @@
+
+
+CREATE TABLE IF NOT EXISTS task
+(
+    id         bigint primary key,
+    title varchar NOT NULL,
+    description TEXT DEFAULT NULL,
+    date_of_accomplishing timestamp with time zone DEFAULT now() NOT NULL,
+    email      varchar(300) NOT NULL,
+    password   varchar(512) NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL
+);
+
+CREATE SEQUENCE task_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE CACHE 1;
