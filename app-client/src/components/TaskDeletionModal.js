2e9e1367d1b54d5246efebe9ffc4ad6370d119d7
Nobumichi Leo Nakamura <we.are.one779@gmail.com>
2022-04-22 13:54:50 -0400

    Created TaskDeleletionModal component

---
+++ app-client/src/components/TaskDeletionModal.js
@@ -0,0 +1,37 @@
+import {Button, Modal} from "react-bootstrap";
+import TaskApiConnection from "../api/TaskApiConnection";
+import {useDispatch} from "react-redux";
+import {taskActions} from "../store/task-slice";
+
+
+const TaskDeletionModal = (props) => {
+
+    const dispatch = useDispatch();
+
+    const deleteTaskHandler = async (event) => {
+        event.preventDefault();
+
+        const deleteRequest = new TaskApiConnection();
+
+        dispatch(taskActions.removeTaskFromTaskList(props.id));
+
+        await deleteRequest.deleteTask(props.id);
+
+        props.onClose();
+    };
+
+    return (
+        <Modal show={props.onShow} onHide={props.onClose}>
+            <Modal.Body>
+                Are you sure you want to delete this task?
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={deleteTaskHandler}>Yes</Button>
+                {"  "}
+                <Button variant="success" onClick={props.onClose}>No</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default TaskDeletionModal;
